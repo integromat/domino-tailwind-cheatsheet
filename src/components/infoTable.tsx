d49e396ae2f803449a49a858aaf193b51e47d4cf
martinsik <m.sikora@make.com>
2024-11-18 16:55:02 +0100

    feat: always show semantic layer name

--- src/components/infoTable.tsx
+++ src/components/infoTable.tsx
@@ -7,7 +7,6 @@ import { LayersContext } from '../utils/layers.context';
 const InfoTable = ({ table } : { table : string[][] }) => {
     const [showToast, setShowToast] = useState(false);
     const [toastText, setToastText] = useState("");
-    const [hoveredRow, setHoveredRow] = useState<number>(-1);
     const layers = useContext(LayersContext);
 
     const parseText = (text: string): any => {
@@ -32,40 +31,33 @@ const InfoTable = ({ table } : { table : string[][] }) => {
         }, 5000)
     }
 
-    function trWithSemanticLayerName(tr: string[], rowIndex: number) {
-        const possibleLayerName = tr[1].replace(/dmo-.+?-/, '');
+    function semanticLayerName(td: string) {
+        const possibleLayerName = td.replace(/dmo-.+?-/, '');
         const layer = Object.keys(layers).find(layerName => layers[layerName].includes(possibleLayerName));
         if (!layer) {
             return null;
         }
 
-        return (
-            <tr key={'hovered-' + rowIndex}
-                onMouseEnter={() => setHoveredRow(rowIndex)}
-                className={"border-b border-gray-300 dark:border-gray-700"}>
-                <td colSpan={100} className={"pb-[2px] pl-2 font-mono text-xs text-gray-400"}>{layer}</td>
-            </tr>);
+        return (<span className={"font-mono text-[8px] text-gray-400"}>{layer}</span>);
     }
 
     return (
         <>
             {showToast && <Toast text={toastText} /> }
             <table className="w-full mb-4 bg-gray-100 rounded dark:bg-neutral-800"
-                   onMouseLeave={() => setHoveredRow(-1)}
             >
                 <tbody>
                 {
                     table.map((tr: string[], rowIndex: number) => {
                         // Only rows with 4 table columns allow hover state, because only those represent a color and
                         // have a semantic layer.
-                        const isHovered = tr.length === 4 && hoveredRow === rowIndex;
+                        const showSemanticLayer = tr.length === 4;
                         return (
                             <>
                                 <tr
                                     key={'tr-' + rowIndex}
-                                    onMouseEnter={() => setHoveredRow(rowIndex)}
                                     className={classNames('border-gray-300 dark:border-gray-700', {
-                                        'border-b': !isHovered,
+                                        'border-b': !showSemanticLayer,
                                     })}
                                 >
                                     {
@@ -80,20 +72,18 @@ const InfoTable = ({ table } : { table : string[][] }) => {
                                                         );
                                                     }}
                                                     key={'td-' + index}
-                                                    rowSpan={isHovered && (index === 0 || index === 3) ? 2 : 1}
-                                                    className={classNames('cursor-copy font-mono text-xs hover:underline px-2', {
+                                                    className={classNames('cursor-copy font-mono text-xs hover:underline p-2', {
                                                         'text-purple-700 dark:text-purple-300 whitespace-nowrap': index === tdIndex(0),
                                                         'text-blue-700 dark:text-blue-300': index ===  tdIndex(1),
                                                         'text-gray-500 dark:text-gray-300 text-xs': index ===  tdIndex(2),
-                                                        'pb-2': !isHovered,
-                                                        'pt-2': !isHovered,
-                                                        'pt-[4px]': isHovered && (index !== 0 && index !== 3),
-                                                    })}>{parseText(td)}</td>
+                                                    })}>
+                                                    {parseText(td)}
+                                                    {(index === 1) && semanticLayerName(td)}
+                                                </td>
                                             );
                                         })
                                     }
                                 </tr>
-                                {isHovered ? trWithSemanticLayerName(tr, rowIndex) : null}
                             </>
                         );
                     })
