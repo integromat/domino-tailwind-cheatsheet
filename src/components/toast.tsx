20119c0c4ef2a814b6c2f3694f39ffbb45a66fcc
vojtesaak <vojtech.malek@gmail.com>
2025-02-07 11:23:29 +0100

    fix: toast color

--- src/components/toast.tsx
+++ src/components/toast.tsx
@@ -4,7 +4,7 @@ export const Toast = ({ text }: { text: string }) => {
 
     return (
         <div
-            className="fixed z-10 p-4 m-4 font-medium text-white rounded-lg shadow-lg top-20 right-6 bg-primary"
+            className="fixed z-10 p-4 m-4 font-medium text-white bg-purple-700 rounded-lg shadow-lg top-20 right-6"
             role="alert"
         >
             <p className="text-sm">"{text}" copied into your clipboard </p>
