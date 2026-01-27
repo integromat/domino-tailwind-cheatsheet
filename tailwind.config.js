e1007d4c1220cab1bdb1b7af6589cf879c6edd4a
martinsik <m.sikora@make.com>
2026-01-27 18:23:39 +0100

    fix some classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,6 @@ module.exports = {
     darkMode: "class",
     theme: {
         extend: {
-            colors: {
-                primary: 'var(--dmo-color-primary-500)',
-                neutral: {
-                    50: 'var(--dmo-color-neutral-50)',
-                    100: 'var(--dmo-color-neutral-100)',
-                    200: 'var(--dmo-color-neutral-200)',
-                    300: 'var(--dmo-color-neutral-300)',
-                    400: 'var(--dmo-color-neutral-400)',
-                    500: 'var(--dmo-color-neutral-500)',
-                    600: 'var(--dmo-color-neutral-600)',
-                    700: 'var(--dmo-color-neutral-700)',
-                    800: 'var(--dmo-color-neutral-800)',
-                    900: 'var(--dmo-color-neutral-900)',
-                }
-            },
             fontFamily: {
                 roboto: ["Roboto", "sans-serif"],
             },
